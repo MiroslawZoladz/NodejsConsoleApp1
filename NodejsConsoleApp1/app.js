cce71cb04faaf24b03e5187697294435e1a9eabe
Mirosław Żołądź <zoladz@agh.edu.pl>
2018-01-24 15:24:39 +0100

    db connection and basic querries works

--- NodejsConsoleApp1/app.js
+++ NodejsConsoleApp1/app.js
@@ -1,3 +1,28 @@
 'use strict';
+var pg = require('pg');
+
+const config = {
+    host: 'mz-node-db.postgres.database.azure.com',
+    user: 'miroslave@mz-node-db',
+    password: 'Amelia12!',
+    database: 'mypgsqldb',
+    port: 5432,
+    ssl: true
+};
+const db = new pg.Client(config);
+
+db.connect(err => {
+    if (err) console.log('DB connect failed');//throw err;
+});
+
+db.query("SELECT * FROM inventory", (err, dbRes) => {
+    if (err) {
+        console.log('Hello world');
+    }
+    else {
+        console.log(dbRes);
+    }
+}
+);
 
 console.log('Hello world');
